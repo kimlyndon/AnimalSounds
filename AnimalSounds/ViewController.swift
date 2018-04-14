3df87e78402e133b51591d4df2fe0ba088de9341
Kim Lyndon <kimmylyndon@gmail.com>
2018-04-13 21:18:45 -0400

    First commit

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
+    
+    @IBAction func catButtonTapped(_ sender: AnyObject) {
+        animalSoundLabel.text = "Meow!"
+        meowSound.play()
+    }
+    
+    
+    @IBAction func dogButtonTapped(_ sender: AnyObject) {
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
+    }
+    
+    @IBAction func cowButtonTapped(_ sender: AnyObject) {
+        animalSoundLabel.text = "Moo!"
+        mooSound.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
